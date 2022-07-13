9a39bc6a3a8945efd9fa6bb9117abeb9be0aa18f
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-07-13 12:23:51 -0500

    interface added; class added to implement

---
+++ SimpleCrm/SimpleCrm/ConfigurationGreeter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCrm
+{
+    public class ConfigurationGreeter : IGreeter
+    {
+        private readonly IConfiguration configuration;
+        public ConfigurationGreeter(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        string IGreeter.GetGreeting()
+        {
+            var message = configuration["Greeting"];
+            return message;
+            
+            // throw new NotImplementedException();
+        }
+    }
+}
