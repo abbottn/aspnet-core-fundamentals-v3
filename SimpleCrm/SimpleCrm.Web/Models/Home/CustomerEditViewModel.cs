8bac30415d2fa24a261e21105103bc11644411a9
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-08-05 11:14:21 -0500

    step20 -add display annotations and validation msg

--- SimpleCrm/SimpleCrm.Web/Models/Home/CustomerEditViewModel.cs
+++ SimpleCrm/SimpleCrm.Web/Models/Home/CustomerEditViewModel.cs
@@ -1,12 +1,33 @@
-﻿namespace SimpleCrm.Web.Models.Home
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.Web.Models.Home
 {
     public class CustomerEditViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "you must provide a first name")]
+        [MinLength(2)]
+        [MaxLength(25)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MinLength(2)]
+        [MaxLength(25)]
+        [Required(ErrorMessage = "you must provide a last name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [MinLength(10)]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "you must provide a phone number")]
         public string PhoneNumber { get; set; }
+
+        [Display(Name = "Opt In Newsletter")]
         public bool OptInNewsletter { get; set; }
+
+        [Display(Name = "Customer Type")]
         public CustomerType Type { get; set; }
 
     }
