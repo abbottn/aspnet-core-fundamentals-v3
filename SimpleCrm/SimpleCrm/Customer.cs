a61cb71b8245e9b02ab8f3df7ab75e3ca5723043
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-08-11 15:39:12 -0500

    step23 - update Entity Framework migration

--- SimpleCrm/SimpleCrm/Customer.cs
+++ SimpleCrm/SimpleCrm/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 
@@ -8,8 +9,13 @@ namespace SimpleCrm
     public class Customer
     {
         public int Id { get; set; }
+        [MaxLength(50)]
+        [Required()]
         public string FirstName { get; set; }
+        [MinLength(1), MaxLength(50)]
+        [Required()]
         public string LastName { get; set; }
+        [MinLength(7), MaxLength(12)]
         public string PhoneNumber { get; set; }
         public CustomerType Type { get; set; }
         public bool OptInNewsletter { get; set; }
