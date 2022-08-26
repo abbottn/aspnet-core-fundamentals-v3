e872db6cbc57e5c1477cadac763a98e74826286c
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-08-25 22:34:26 -0500

    step34 - Setup User Registration Form/Controller

---
+++ SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.Web.Models.Account;
+
+namespace SimpleCrm.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+               
+
+            }
+            return NoContent(); // TODO: validate model, register the user
+        }
+    }  
+}
---
+++ SimpleCrm/SimpleCrm.Web/Models/Account/RegisterUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.Web.Models.Account
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [DisplayName("User")]
+        public string UserName { get; set; }
+
+        [Required, MaxLength(256), DisplayName("Name")]
+        public string DisplayName { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords Don't Match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
