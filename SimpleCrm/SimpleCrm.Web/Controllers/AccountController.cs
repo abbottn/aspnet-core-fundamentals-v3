afd06c39ab1c185c153da585048debc88ff5ff68
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-08-29 14:43:58 -0500

    step35 - Controller action for user registration

--- SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SimpleCrm.Web.Models.Account;
+using System.Threading.Tasks;
 
 namespace SimpleCrm.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<CrmUser> userManager;
+        private readonly SignInManager<CrmUser> signInManager;
+
+        public AccountController(UserManager<CrmUser> userManager, 
+            SignInManager<CrmUser> signInManager
+            )
+        { // injection is done in the "constructor" method of the class
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+         
         [HttpGet]
         public IActionResult Register()
         {
@@ -12,14 +25,32 @@ namespace SimpleCrm.Web.Controllers
         }
 
         [HttpPost, ValidateAntiForgeryToken]
-        public IActionResult Register(RegisterUserViewModel model)
+        public async Task<IActionResult> Register(
+            RegisterUserViewModel model)
         {
             if (ModelState.IsValid)
-            {
-               
-
+            { // model is valid, DataAnnotation validators have passed
+                var user = new CrmUser
+                {
+                    UserName = model.UserName,
+                    DisplayName = model.DisplayName,
+                    Email = model.UserName
+                };
+                var CreateResult = await userManager.CreateAsync(user, model.Password);
+                if (CreateResult.Succeeded)
+                {
+                    //TODO: user is created, let's log them in and redirect
+                    await this.signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                //TODO: failed to create, let's tell the user why and let them fix it 
+                foreach (var result in CreateResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, result.Description);
+                }
+                // return NoContent(); // TODO: validate model, register the user remove this line when done
             }
-            return NoContent(); // TODO: validate model, register the user
+            return View(); // show the view again with validation errors highlighted
         }
     }  
 }
