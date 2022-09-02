7fb33908789f2439c66e2027fa259dc533839745
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-09-02 12:28:18 -0500

    step36 - User Login feature added and tested

---
+++ SimpleCrm/SimpleCrm.Web/Models/Account/LoginUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.Web.Models.Account
+{
+    public class LoginUserViewModel
+    {
+        [Required]
+        [DisplayName("User")]
+        public string UserName { get; set; }
+        [Required]
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Stay Logged In")]
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
