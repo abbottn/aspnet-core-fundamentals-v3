9ece26cedb69186abd68cfe507b322f027a7fc7e
Nicholas Abbott <Nicholas.Abbott.BSEE@gmail.com>
2022-09-30 11:58:43 -0500

    54: customer list table on a card, max content wth

--- SimpleCrm/simplecrm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
+++ SimpleCrm/simplecrm-cli/src/app/customer/customer-list-page/customer-list-page.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+  import { Customer } from '../customer.model';
+  import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
-@Component({
-  selector: 'crm-customer-list-page',
-  templateUrl: './customer-list-page.component.html',
-  styleUrls: ['./customer-list-page.component.scss']
-})
-export class CustomerListPageComponent implements OnInit {
+  @Component({
+    selector: 'crm-customer-list-page',
+    templateUrl: './customer-list-page.component.html',
+    styleUrls: ['./customer-list-page.component.scss']
+  })
+  export class CustomerListPageComponent implements OnInit {
+    customers: Customer[] = [
+      {
+        customerId: 1,
+        firstName: 'John',
+        lastName: 'Smith',
+        phoneNumber: '314-555-1234',
+        emailAddress: 'john@nexulacademy.com',
+        statusCode: 'Prospect',
+        preferredContactMethod: 'phone',
+        lastContactDate: new Date().toISOString()
+      },
+      {
+        customerId: 2,
+        firstName: 'Tory',
+        lastName: 'Amos',
+        phoneNumber: '314-555-9873',
+        emailAddress: 'tory@example.com',
+        statusCode: 'Prospect',
+        preferredContactMethod: 'email',
+        lastContactDate: new Date().toISOString()
+      }
+    ];
+    dataSource!: MatTableDataSource<Customer>; // The ! tells Angular you know it may be used before it is set.  Try it without to see the error
+    // @ViewChild(MatSort, {static: true}) sort: MatSort;
+    displayColumns = ['name', 'phoneNumber', 'emailAddress', 'status'];
+    // the above column names must match the matColumnDef names in the html
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+      this.dataSource = new MatTableDataSource(this.customers);
+      // this.dataSource.sort = this.sort;
+    }
   }
-
-}
